daa0dde95bec4e17d2597debc406dd4f9cfa7885
Dhruv Daberao <dhruvdaberao@gmail.com>
2026-06-01 22:27:07 +0530

    style: apply premium mobile UI improvements

--- src/components/BootstrapNavbar.tsx
+++ src/components/BootstrapNavbar.tsx
@@ -32,8 +32,8 @@ export default function BootstrapNavbar() {
             <Image
               src="/uploads/hero/logo.png"
               alt="Keshvi Crafts Logo"
-              width={160}
-              height={56}
+              width={120}
+              height={40}
               className="logo-img"
               priority={true}
               style={{ objectFit: "contain" }}
